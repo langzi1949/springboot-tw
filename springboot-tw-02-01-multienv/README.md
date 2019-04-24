91a922bfed6fd4643fe5f8c4cb2582a9ea37a64e
chenzehao <chenzehao258@hfjy.com>
2019-04-24 11:50:25 +0800

    multienv example :dev test prod

--- springboot-tw-02-01-multienv/README.md
+++ springboot-tw-02-01-multienv/README.md
@@ -2,8 +2,15 @@
 
 ```text
 本项目是基于SpringBoot 2.x构建的项目。
-遇到的坑 ：
-    1. Gradle的配置文件，编写过程中有个MavenLocal() 配置的URL有问题
-    2. @SpringApplicationTest 已经在2.x配弃用了，应该直接使用SpringBootTest
+多环境的配置信息如何使用：
+    一般来说，会在resource文件夹下建 application-dev application-test applicaiton-prod等相关的
+    properties文件，在application.properties中 使用  spring.profiles.active = dev 进行标志
 
 ```
+
+这个项目遇到的一个坑就是使用了lombok插件，简化了Getter/Setter等相关的代码。
+需要再Gradle配置文件中添加相关的依赖，具体的请参考`build.gradle`
+
+PS：如果遇到读取配置文件乱码的话，请在配置文件中添加编码配置。
+
+
--- springboot-tw-02-01-multienv/src/main/java/com/zmglove/service/Apiproperties.java
+++ springboot-tw-02-01-multienv/src/main/java/com/zmglove/service/Apiproperties.java
@@ -1,11 +1,31 @@
 package com.zmglove.service;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
- * TODO
+ * Api的配置信息
  *
  * @author CZH
  * @version 1.0
  * @date 2019/4/22 17:28
  **/
+@Component
+@Data
 public class Apiproperties {
+    @Value("${com.zmglove.tw.apiUri}")
+    private String apiUri;
+
+    @Value("${com.zmglove.tw.nameSpace}")
+    private String nameSpace;
+
+    @Value("${com.zmglove.tw.desc}")
+    private String desc;
+
+    @Value("${com.zmglove.tw.value}")
+    private String value;
+
+    @Value("${com.zmglove.tw.number}")
+    private Integer number;
 }

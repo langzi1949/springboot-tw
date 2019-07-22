6a6d2675524916bc7185cb4bf0a750586d7bcc9d
chenzehao <chenzehao_1949@126.com>
2019-07-22 17:06:17 +0800

    暂存

--- springboot-tw-02-02-config/src/main/java/com/zmglove/service/Mp4Service.java
+++ springboot-tw-02-02-config/src/main/java/com/zmglove/service/Mp4Service.java
@@ -1,11 +1,33 @@
 package com.zmglove.service;
 
+import org.springframework.stereotype.Service;
+import ws.schild.jave.EncoderException;
+import ws.schild.jave.MultimediaInfo;
+import ws.schild.jave.MultimediaObject;
+
+import java.io.File;
+
 /**
- * TODO
+ * 处理MP4视频
  *
  * @author CZH
  * @version 1.0
  * @date 2019/7/19 19:45
  **/
+@Service
 public class Mp4Service {
+
+    public long getTime(String videoPath) throws EncoderException {
+        MultimediaInfo info = getMediaInfo(videoPath);
+        return info.getDuration();
+    }
+
+
+    public MultimediaInfo getMediaInfo(String videoPath) throws EncoderException {
+        File source = new File(videoPath);
+        // 创建媒体对象
+        MultimediaObject multimediaObject = new MultimediaObject(source);
+        // 创建信息对象
+        return multimediaObject.getInfo();
+    }
 }
